714a11c25a47a47d4576191e27c919bdededdcac
ggottemo <ggottemo717@gmail.com>
2023-04-15 17:52:32 -0400

    Update blog styling

--- src/routes/blogs.jsx
+++ src/routes/blogs.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import NavBar from "../components/NavBar.jsx";
+import CxFreeze from "../mdx/cx_freeze/CxFreeze.mdx";
 
-// const importModules = import.meta.glob('../mdx/*.mdx', { import: 'Post' })
+//const importModules = import.meta.glob("../mdx/*.mdx", { import:
+// "Post" });
 const blogs = () => {
-    return (
-        <div>
-            <NavBar />
-            <div id="blogs-container">
-                <h1>Coming Soon!</h1>
-
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavBar />
+      <div id="blogs-container">
+        <CxFreeze />
+      </div>
+    </div>
+  );
+};
 
 export default blogs;
