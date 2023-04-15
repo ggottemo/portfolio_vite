0cd4efe216a0db86f67bd8b12b49fcb3abb0d0fb
ggottemo <ggottemo717@gmail.com>
2023-04-15 17:52:11 -0400

    Add blog content

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
+import scss from "rollup-plugin-scss";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    {enforce: 'pre', ...mdx()},react()],
-})
+    {
+      enforce: "pre",
+      ...mdx({ remarkPlugins: [remarkMdxFrontmatter] }),
+    },
+    scss(),
+
+    react(),
+  ],
+});
